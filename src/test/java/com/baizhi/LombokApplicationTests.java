4a1b1df999a5611224a98c0d68d5b3881ebb81d5
ld <1586087602@qq.com>
2019-03-13 11:53:45 +0800

    分工合作的项目

---
+++ src/test/java/com/baizhi/LombokApplicationTests.java
@@ -0,0 +1,24 @@
+package com.baizhi;
+
+import com.baizhi.dao.AdminMapper;
+import com.baizhi.entity.Admin;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LombokApplicationTests {
+
+    @Autowired
+    private AdminMapper adminMapper;
+
+    @Test
+    public void contextLoads() {
+        Admin admin = adminMapper.selectByPrimaryKey("1");
+        System.out.println(admin);
+    }
+
+}
